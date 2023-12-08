8b91c3e7592f5d0c3b903ef910e83128913cf7d0
Iash Bashir <Iash.Bashir@sea.gloal.com>
2023-12-08 15:47:54 +0800

    WIP - merged df

--- project/script/rocket.py
+++ project/script/rocket.py
@@ -53,18 +53,96 @@ class Rocket:
             print(f"An error occurred while reading the CSV file: {e}")
             return None
 
-    def extract_comments(self):
-        # Implementation of extract_comments
-        pass
+    def extract_time(text: str) -> float:
+        """Extract the numerical value of time from a string containing 't ='
 
+        Args:
+            text (str): string containing 't ='
+
+        Returns:
+            float: numerical value of time or else None
+        """
+        match = re.search(r"t=([\d\.]+)", text)
+        return float(match.group(1)) if match else None
+
+    def remove_non_caps(text: str) -> str:
+        """Removes all words that are not in all caps from the given text.
+
+        Args:
+            text (str): The string from which to remove non-uppercase words
+
+        Returns:
+            str: Modified string containing only uppercase words
+        """
+        return " ".join(word for word in text.split() if word.isupper())
+
+    def extract_comments(self):
+        """
+        Reads a CSV file and extracts rows that contain comments in the '# Time (s)' column.
+        """
+        df = self.df.__deepcopy__
+        comments_df = df[df["# Time (s)"].astype(str).str.contains("#")].__deepcopy__()
+        comments_df["Time (s)"] = comments_df["# Time (s)"].apply(
+            lambda x: re.search(r"t=([\d\.]+)", x).group(1)
+            if re.search(r"t=([\d\.]+)", x)
+            else None
+        )
+        comments_df["Time (s)"] = pd.to_numeric(
+            comments_df["Time (s)"], errors="coerce"
+        )
+        comments_df["Event"] = (
+            comments_df["# Time (s)"]
+            .str.replace(r"occurred at t=[\d\.]+ seconds", "")
+            .str.replace("#", "")
+            .str.strip()
+        )
+        # Apply the function to the 'Event' column of your DataFrame
+        comments_df["Event"] = comments_df["Event"].apply(self.remove_non_caps)
+
+        # Rename 'LAUNCH' to 'LAUNCH/IGNITION'
+        comments_df["Event"] = comments_df["Event"].replace(
+            {"LAUNCH": "LAUNCH/IGNITION"}
+        )
+        # Rename 'BURNOUT' to 'BURNOUT/EJECTION_CHARGE'
+        comments_df["Event"] = comments_df["Event"].replace(
+            {"BURNOUT": "BURNOUT/EJECTION_CHARGE"}
+        )
+        # Rename 'GROUND_HIT' to 'GROUND_HIT/SIMULATION_END'
+        comments_df["Event"] = comments_df["Event"].replace(
+            {"GROUND_HIT": "GROUND_HIT/SIMULATION_END"}
+        )
+
+        # Remove the 'IGNITION' row
+        comments_df = comments_df[comments_df["Event"] != "IGNITION"]
+        # Remove the 'EJECTION_CHARGE' row
+        comments_df = comments_df[comments_df["Event"] != "EJECTION_CHARGE"]
+        # Remove the 'SIMULATION_END' row
+        comments_df = comments_df[comments_df["Event"] != "SIMULATION_END"]
+
+        return comments_df[["Time (s)", "Event"]]
+
+    # TODO
     def filter_comments_from_csv(self):
         # Implementation of filter_comments_from_csv
         pass
 
+    # TODO
     def merge_dataframes(self):
         # Implementation of merge_dataframes
-        pass
+        """
+        Merges the filtered DataFrame with the comments DataFrame.
+        """
+        merged_df = self.filtered_df.merge(self.comments_df, on="Time (s)", how="left")
+        # Dropping the 'Time (s)' column from the merged DataFrame
+        merged_df.drop(columns=["Time (s)"], inplace=True)
 
+        # Ensure 'Time (s)' in filtered_df is float, if it's not already
+        merged_df["# Time (s)"] = pd.to_numeric(
+            merged_df["# Time (s)"], errors="coerce"
+        )
+        return merged_df
+
+    # TODO
     def plot_Flight_Profile(self):
         # Implementation of plot_Flight_Profile
         pass
@@ -83,14 +161,12 @@ class Rocket:
         ]
         return float(event_times.iloc[0]) if not event_times.empty else None
 
-    def remove_non_caps(self, text):
-        # Implementation of remove_non_caps
-        pass
-
+    # TODO
     def remove_time_phrase(self, df):
         # Implementation of remove_time_phrase
         pass
 
+    # TODO
     def run(self):
         # Optionally, you can create a run method to execute the main logic
         self.plot_Flight_Profile(self.merged_df)
