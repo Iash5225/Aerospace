67cbf924f1278c387f6d7f6335e9741b6c44d03c
Iash Bashir <Iash.Bashir@sea.gloal.com>
2023-12-15 10:47:38 +0800

    WIP - filtered df

--- project/script/data_handler.py
+++ project/script/data_handler.py
@@ -3,172 +3,94 @@ import re
 
 
 class DataHandler:
-    """Handles data operations for Rocket analysis."""
+    """Handles data operations for rocket analysis."""
 
     def __init__(self, filepath):
         """
         Initialize the DataHandler with a given file path.
-
         Args:
             filepath (str): Path to the CSV file.
         """
         self.filepath = filepath
         self.df = None
         self.comments_df = None
+        self.merged_df = None
+        self.filtered_df = None
 
     def read_OR_csv(self):
-        """Reads the CSV file and stores it in a DataFrame."""
+        """Reads the Open Rocket CSV file and stores it in a DataFrame."""
         try:
             self.df = pd.read_csv(self.filepath, delimiter=",", skiprows=6)
+            self._prepare_dataframes()
         except Exception as e:
-            print(f"An error occurred while reading the CSV file: {e}")
+            print(f"Error reading the CSV file: {e}")
 
-    def filter_comments(self):
-        """Extracts comments from the DataFrame and stores them in a separate DataFrame."""
-        if self.df is None:
-            print("Data not loaded. Please call read_csv first.")
-            return
+    def _prepare_dataframes(self):
+        """Prepares data and comments dataframes."""
+        self._filter_comments()
+        self.filtered_df = self._filter_data()
+        self.merged_df = self._merge_dataframes()
 
-        comments_df = self.df[self.df["# Time (s)"].astype(
-            str).str.contains("#")].copy()
+    def _filter_comments(self):
+        """Filters comments from the main DataFrame and stores them separately."""
+        comments_mask = self.df["# Time (s)"].astype(str).str.contains("#")
+        comments_df = self.df[comments_mask].copy()
         comments_df["Time (s)"] = comments_df["# Time (s)"].apply(
             self._extract_time)
         comments_df["Event"] = comments_df["# Time (s)"].apply(
             self._extract_event)
-        self.comments_df = comments_df[["Time (s)", "Event"]]
+        self.comments_df = self._process_comment_events(comments_df)
 
-    def merge_dataframes(self) -> pd.DataFrame:
-        """Merges the filtered DataFrame with the comments DataFrame.
-
-        Returns:
-            pd.DataFrame:  DataFrame containing the merged data
-        """
-
-        merged_df = self.filtered_df.merge(
-            self.comments_df, on="Time (s)", how="left")
-        # Dropping the 'Time (s)' column from the merged DataFrame
-        merged_df.drop(columns=["Time (s)"], inplace=True)
-
-        # Ensure 'Time (s)' in filtered_df is float, if it's not already
-        merged_df["# Time (s)"] = pd.to_numeric(
-            merged_df["# Time (s)"], errors="coerce"
-        )
-
-        rename_dict = {"# Time (s)": "Time (s)"}
-        merged_df.rename(columns=rename_dict, inplace=True)
-
-        return merged_df
-
-    def filter_comments_from_csv(self) -> pd.DataFrame:
-        """Filters out rows that contain comments from the dataframe.
-
-        Returns:
-            pd.DataFrame:  DataFrame containing the filtered data
-        """
-        df = self.df.copy(deep=True)
-        filtered_df = df[~df["# Time (s)"].astype(str).str.contains("#")].copy(
-            deep=True
-        )
-        filtered_df["Time (s)"] = pd.to_numeric(
-            filtered_df["# Time (s)"], errors="coerce"
-        )
+    def _filter_data(self) -> pd.DataFrame:
+        """Filters out comment rows from the main DataFrame."""
+        filtered_df = self.df[~self.df["# Time (s)"].astype(
+            str).str.contains("#")].copy()
+        print(filtered_df[["# Time (s)"]])
+        filtered_df.rename(columns={"# Time (s)": "Time (s)"},inplace=True)
+        print(filtered_df["Time (s)"])
         return filtered_df
 
-    def _extract_time(self, text):
+    def _merge_dataframes(self) -> pd.DataFrame:
+        """Merges filtered data with comments data."""
+        merged = self.filtered_df.merge(self.comments_df, on="Time (s)", how="left")
+        merged["Time (s)"] = pd.to_numeric(
+            merged["# Time (s)"], errors="coerce")
+        return merged.rename(columns={"# Time (s)": "Time (s)"})
+
+    def _extract_time(self, text: str) -> float:
         """Extracts time from a comment string."""
         match = re.search(r"t=([\d\.]+)", text)
         return float(match.group(1)) if match else None
 
-    def _extract_event(self, text):
+    def _extract_event(self, text: str) -> str:
         """Extracts event name from a comment string."""
-        # Further processing can be added here based on your requirements
-        return text
+        return text.replace(r"occurred at t=[\d\.]+ seconds", "").replace("#", "").strip()
 
-    def remove_non_caps(self, text: str) -> str:
-        """Removes all words that are not in all caps from the given text.
-
-        Args:
-            text (str): The string from which to remove non-uppercase words
-
-        Returns:
-            str: Modified string containing only uppercase words
-        """
-        return " ".join(word for word in text.split() if word.isupper())
-
-    def extract_comments(self) -> pd.DataFrame:
-        """Extracts the comments from the CSV file and returns a DataFrame.
-
-        Returns:
-            pd.DataFrame:  DataFrame containing the comments from the CSV file
-        """
-        df = self.df
-        comments_df = df[df["# Time (s)"].astype(
-            str).str.contains("#")].copy(deep=True)
-        comments_df["Time (s)"] = comments_df["# Time (s)"].apply(
-            lambda x: re.search(r"t=([\d\.]+)", x).group(1)
-            if re.search(r"t=([\d\.]+)", x)
-            else None
-        )
-        comments_df["Time (s)"] = pd.to_numeric(
-            comments_df["Time (s)"], errors="coerce"
-        )
-        comments_df["Event"] = (
-            comments_df["# Time (s)"]
-            .str.replace(r"occurred at t=[\d\.]+ seconds", "")
-            .str.replace("#", "")
-            .str.strip()
-        )
-        # Apply the function to the 'Event' column of your DataFrame
+    def _process_comment_events(self, comments_df: pd.DataFrame) -> pd.DataFrame:
+        """Processes events in the comments dataframe."""
         comments_df["Event"] = comments_df["Event"].apply(self.remove_non_caps)
-
-        # Rename 'LAUNCH' to 'LAUNCH/IGNITION'
-        comments_df["Event"] = comments_df["Event"].replace(
-            {"LAUNCH": "LAUNCH/IGNITION"}
-        )
-        # Rename 'BURNOUT' to 'BURNOUT/EJECTION_CHARGE'
-        comments_df["Event"] = comments_df["Event"].replace(
-            {"BURNOUT": "BURNOUT/EJECTION_CHARGE"}
-        )
-        # Rename 'GROUND_HIT' to 'GROUND_HIT/SIMULATION_END'
-        comments_df["Event"] = comments_df["Event"].replace(
-            {"GROUND_HIT": "GROUND_HIT/SIMULATION_END"}
-        )
-
-        # Remove the 'IGNITION' row
-        comments_df = comments_df[comments_df["Event"] != "IGNITION"]
-        # Remove the 'EJECTION_CHARGE' row
-        comments_df = comments_df[comments_df["Event"] != "EJECTION_CHARGE"]
-        # Remove the 'SIMULATION_END' row
-        comments_df = comments_df[comments_df["Event"] != "SIMULATION_END"]
-
+        events_to_replace = {
+            "LAUNCH": "LAUNCH/IGNITION",
+            "BURNOUT": "BURNOUT/EJECTION_CHARGE",
+            "GROUND_HIT": "GROUND_HIT/SIMULATION_END"
+        }
+        events_to_remove = ["IGNITION", "EJECTION_CHARGE", "SIMULATION_END"]
+        comments_df["Event"] = comments_df["Event"].replace(events_to_replace)
+        comments_df = comments_df[~comments_df["Event"].isin(events_to_remove)]
         return comments_df[["Time (s)", "Event"]]
 
     def find_event_time(self, event_name: str) -> float:
-        """Find the time when a specific event occurred.
-
-        Args:
-            event_name (str): The name of the event to find
-
-        Returns:
-            float: The time when the event occurred or None if the event was not found.
-        """
-        event_times = self.merged_df.loc[
-            self.merged_df["Event"] == event_name, "Time (s)"
-        ]
-        return float(event_times.iloc[0]) if not event_times.empty else None
+        """Finds the time when a specific event occurred."""
+        event_time = self.merged_df.loc[self.merged_df["Event"]
+                                        == event_name, "Time (s)"]
+        return float(event_time.iloc[0]) if not event_time.empty else None
 
     def find_event_mach(self, event_name: str) -> float:
-        """Find the time when a specific event occurred.
+        """Finds the Mach number when a specific event occurred."""
+        event_mach = self.merged_df.loc[self.merged_df["Event"]
+                                        == event_name, "Mach number (​)"]
+        return float(event_mach.iloc[0]) if not event_mach.empty else None
 
-        Args:
-            event_name (str): The name of the event to find
-
-        Returns:
-            float: The time when the event occurred or None if the event was not found.
-        """
-        event_times = self.merged_df.loc[
-            self.merged_df["Event"] == event_name, "Mach number (​)"
-        ]
-        return float(event_times.iloc[0]) if not event_times.empty else None
-
-    # Additional methods can be added here for more data operations
+    def remove_non_caps(self, text: str) -> str:
+        """Removes all words that are not in all caps from the given text."""
+        return " ".join(word for word in text.split() if word.isupper())
---
+++ project/script/main.py
@@ -0,0 +1,25 @@
+from data_handler import DataHandler
+import os
+
+def main():
+    # Usage
+    data_file_name = "Rocket Data.csv"
+    # Get the directory of the current script
+    script_dir = os.path.dirname(__file__)
+    # Go up one level to the project directory
+    project_dir = os.path.join(script_dir, "..")
+    # Construct the path to the CSV file
+    csv_path = os.path.join(project_dir, "data", data_file_name)
+    # Normalize the path (optional but recommended)
+    csv_path = os.path.normpath(csv_path)
+    
+    data_handler = DataHandler(csv_path)
+    data_handler.read_OR_csv()
+    print(data_handler.filtered_df)
+    print(data_handler.comments_df)
+    # print(data_handler.merged_df)
+    # data_handler.filter_comments()
+    
+
+if __name__ == "__main__":
+    main()
