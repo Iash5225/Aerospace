56dca0868a1d702ce1aa6af987a29c17bf8ce348
Iash Bashir <Iash.Bashir@sea.gloal.com>
2023-12-19 16:16:10 +0800

    Refactor flutter velocity calculation and add input parameters

--- project/script/sahr_fin_flutter.py
+++ project/script/sahr_fin_flutter.py
@@ -10,18 +10,12 @@ B_expr = b**2 / S_expr
 lambda_expr = ct / cr
 T_expr = t/cr
 
-# Expressions for atmospheric properties
-# P_expr = 2116/144 * ((T + 459.7) / 518.6) ** 5.256
-# a_expr = (1.4 * 1716.59 * (T + 460))**0.5
-
 # Fin flutter velocity expression
 Vf_expr = 1.223 * Cs0 * exp(0.4*h/H)*sqrt(G/P0) * \
     sqrt((2+B)/(1+lambda_ratio))*(T/B)**(3/2)
 
 # Lambdify expressions for numerical calculations
-normalised_thickness_function = lambdify(h, T_expr, modules='numpy')
-# pressure_function = lambdify(T, P_expr, modules='numpy')
-# speed_of_sound_function = lambdify(T, a_expr, modules='numpy')
+normalised_thickness_function = lambdify((t, cr), T_expr, modules='numpy')
 aspect_ratio_function = lambdify((cr, ct, b), B_expr, modules='numpy')
 taper_ratio_function = lambdify((cr, ct), lambda_expr, modules='numpy')
 flutter_velocity_lambdified = lambdify(
@@ -30,28 +24,17 @@ flutter_velocity_lambdified = lambdify(
 # Now, you can call these lambdified functions in your main calculation functions
 
 
-def calculate_temperature(altitude):
-    return temperature_function(altitude)
+def calculate_normalised_thickness(thickness, root_chord):
+    return normalised_thickness_function(thickness, root_chord)
 
 
-def calculate_pressure(altitude):
-    temperature = calculate_temperature(altitude)
-    return pressure_function(temperature)
-
-
-def calculate_speed_of_sound(altitude):
-    temperature = calculate_temperature(altitude)
-    return speed_of_sound_function(temperature)
-
-
-def calculate_flutter_velocity(altitude, shear_modulus, thickness, root_chord, tip_chord, semispan):
+def calculate_flutter_velocity(sea_level_speed_of_sound, altitude, atmospheric_scale_height, shear_modulus, sea_level_pressure, thickness, root_chord, tip_chord, semispan):
     aspect_ratio = aspect_ratio_function(root_chord, tip_chord, semispan)
     taper_ratio = taper_ratio_function(root_chord, tip_chord)
-    pressure = calculate_pressure(altitude)
-    speed_of_sound = calculate_speed_of_sound(altitude)
+    normalised_thickness = calculate_normalised_thickness(thickness=thickness,root_chord=root_chord)
 
     flutter_velocity = flutter_velocity_lambdified(
-        speed_of_sound, shear_modulus, aspect_ratio, pressure, taper_ratio, thickness, root_chord)
+        sea_level_speed_of_sound, altitude, atmospheric_scale_height, shear_modulus, sea_level_pressure, aspect_ratio, taper_ratio, normalised_thickness)
 
     return flutter_velocity
 
@@ -66,10 +49,14 @@ def main():
     root_chord = 9.75  # in inches
     tip_chord = 3.75  # in inches
     semispan = 4.75  # in inches
+    sea_level_sound = 1100 # in ft/s
+    sea_level_pressure = 14.7 # in psi
+    atmospheric_scale_height = 26500 #ft
+    
 
     # Perform calculations
     flutter_velocity = calculate_flutter_velocity(
-        altitude, shear_modulus, thickness, root_chord, tip_chord, semispan)
+        sea_level_sound, altitude, atmospheric_scale_height, shear_modulus, sea_level_pressure,thickness,root_chord,tip_chord,semispan)
 
     # Output results
     print(
