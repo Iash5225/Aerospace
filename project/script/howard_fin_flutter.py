b12aa82add5b1b8bb21519f105b2e8892110c054
Iash Bashir <Iash.Bashir@sea.gloal.com>
2023-12-20 10:47:49 +0800

    Add type annotations and docstrings to altitude-related functions

--- project/script/howard_fin_flutter.py
+++ project/script/howard_fin_flutter.py
@@ -28,21 +28,65 @@ flutter_velocity_lambdified = lambdify(
         (a, G, AR, P, lambda_ratio, t, cr), Vf_expr, modules='numpy')
 
 # Now, you can call these lambdified functions in your main calculation functions
-def calculate_temperature(altitude):
+def calculate_temperature(altitude:float)->float:
+    """
+    calculate_temperature  Calculate the temperature at a given altitude in Fahrenheit
+
+    Args:
+        altitude (float):  The altitude in feet
+
+    Returns:
+        float:  The temperature in Fahrenheit
+    """    
+
     return temperature_function(altitude)
 
 
-def calculate_pressure(altitude):
+def calculate_pressure(altitude:float)->float:
+    """
+    calculate_pressure  Calculate the pressure at a given altitude in psi
+
+    Args:
+        altitude (float):  The altitude in feet
+
+    Returns:
+        float:  The pressure in psi
+    """    
+     
     temperature = calculate_temperature(altitude)
     return pressure_function(temperature)
 
 
-def calculate_speed_of_sound(altitude):
+def calculate_speed_of_sound(altitude:float)->float:
+    """
+    calculate_speed_of_sound  Calculate the speed of sound at a given altitude in ft/s
+
+    Args:
+        altitude (float):  The altitude in feet
+
+    Returns:
+        float:  The speed of sound in ft/s
+    """     
+    
     temperature = calculate_temperature(altitude)
     return speed_of_sound_function(temperature)
 
 
-def calculate_flutter_velocity(altitude, shear_modulus, thickness, root_chord, tip_chord, semispan):
+def calculate_flutter_velocity(altitude:float, shear_modulus:float, thickness:float, root_chord:float, tip_chord:float, semispan:float)->float:
+    """
+    calculate_flutter_velocity  Calculate the flutter velocity at a given altitude in ft/s
+
+    Args:
+        altitude (float):  The altitude in feet
+        shear_modulus (float):  The shear modulus in psi
+        thickness (float):  The thickness in inches
+        root_chord (float):  The root chord in inches
+        tip_chord (float):  The tip chord in inches
+        semispan (float):  The semispan in inches
+
+    Returns:
+        float:  The flutter velocity in ft/s
+    """    
     aspect_ratio = aspect_ratio_function(root_chord, tip_chord, semispan)
     taper_ratio = taper_ratio_function(root_chord, tip_chord)
     pressure = calculate_pressure(altitude)
